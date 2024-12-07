ecb7f8301cc89c0c25daab99448b2041b36fe17b
Harsh Sharma <xoxoharsh@Harshs-Laptop.local>
2024-12-08 02:35:41 +0530

    flowbite setup and routing setup is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Events from "./pages/Events";
+import Register from "./pages/Register";
+import Sponsors from "./pages/Sponsors";
+import Team from "./pages/Team";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div className="app-component">App</div>
+      <div className="main_area">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/team" element={<Team />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   );
 };
